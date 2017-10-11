79d6c12f43a8291b669412f800bc2d48ec3d6b35
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-10-11 16:50:08 -0600

    done

--- script.js
+++ script.js
@@ -1,48 +1,77 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-
-
-
-// make a div with the incoming color
-function makeDiv(inColor) {
-  let myDiv = document.createElement("div");
-  myDiv.style.backgroundColor = inColor;
-  myDiv.style.paddingBottom = "11.1%";
-  myDiv.style.width = "11.1%";
-  myDiv.style.float = "left";
-  return myDiv;
-}
-
-//  make a row
-function makeRow(firstColor, secondColor) {
-
-  //  make a div container to hold a row
-  let myContainerDiv = document.createElement("div");
-
-  for (let i = 0; i < 8; i++) {
-    let myDiv = makeDiv(firstColor);
-    if (i === 0) {
-      myDiv.style.clear = "left";
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-     myContainerDiv.appendChild(myDiv);
-     [firstColor, secondColor] = [secondColor, firstColor];
+    return color;
   }
-  return myContainerDiv;
-}
 
-//  make the grid
-function makeGrid() {
-  let firstColor = "#ff0000";
-  let secondColor = "#000000";
 
-  let myBody = document.querySelector("body");
 
-  for (let i = 0; i < 8; i++) {
-    myBody.appendChild(makeRow(firstColor, secondColor));
-    [firstColor, secondColor] = [secondColor, firstColor];
+  // make a div with the incoming color
+  function makeDiv(inColor) {
+    let myDiv = document.createElement("div");
+    myDiv.style.backgroundColor = inColor;
+    myDiv.style.paddingBottom = "11.1%";
+    myDiv.style.width = "11.1%";
+    myDiv.style.float = "left";
+    return myDiv;
   }
-}
 
-makeGrid();
+  //  make a row that alternates colors
+  function makeRow(firstColor, secondColor) {
+
+    //  make a div container to hold a row
+    let myContainerDiv = document.createElement("div");
+
+    for (let i = 0; i < 8; i++) {
+      let myDiv = makeDiv(firstColor);
+      if (i === 0) {
+        myDiv.style.clear = "left";
+      }
+      myContainerDiv.appendChild(myDiv);
+      [firstColor, secondColor] = [secondColor, firstColor];
+    }
+    return myContainerDiv;
+  }
+
+  //  make a row that alternates colors
+  function makeRowOfRandomColors() {
+
+    //  make a div container to hold a row
+    let myContainerDiv = document.createElement("div");
+
+    for (let i = 0; i < 8; i++) {
+      let myDiv = makeDiv(getRandomColor());
+      if (i === 0) {
+        myDiv.style.clear = "left";
+      }
+      myContainerDiv.appendChild(myDiv);
+    }
+    return myContainerDiv;
+  }
+
+  function makeGridOfAlternatingColors(firstColor, secondColor) {
+    let myBody = document.querySelector("body");
+
+    for (let i = 0; i < 8; i++) {
+      myBody.appendChild(makeRow(firstColor, secondColor));
+      [firstColor, secondColor] = [secondColor, firstColor];
+    }
+  }
+
+  function makeGridOfRandomColors() {
+    let myBody = document.querySelector("body");
+
+    for (let i = 0; i < 8; i++) {
+      myBody.appendChild(makeRowOfRandomColors());
+    }
+  }
+
+  // makeGridOfAlternatingColors("#ff0000", "#000000");
+  makeGridOfRandomColors();
 
 });
