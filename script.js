4cb7ec4ac9cb200df3adba27e5e0488b6b1db865
llamacoder <30612433+llamacoder@users.noreply.github.com>
2017-10-11 16:02:28 -0600

    red and black single squares working

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// Your JS goes here
+document.addEventListener("DOMContentLoaded", function() {
+
+
+
+
+// make a div with the incoming color
+function makeDiv(inColor) {
+  let myDiv = document.createElement("div");
+  myDiv.style.backgroundColor = inColor;
+  myDiv.style.paddingBottom = "11.1%";
+  myDiv.style.width = "11.1%";
+  myDiv.style.float = "left";
+  return myDiv;
+}
+
+//  make a row
+function makeRow() {
+  //  get the body
+  let myBody = document.querySelector("body");
+
+  //  make a div container to hold a row
+  let myContainerDiv = document.createElement("div");
+
+  let elem1 = makeDiv("#ff0000");
+  let elem2 = makeDiv("#000000");
+  myBody.appendChild(elem1);
+  myBody.appendChild(elem2);
+}
+
+//  make the grid
+function makeGrid() {
+  makeRow();
+}
+
+makeGrid();
+
+});
